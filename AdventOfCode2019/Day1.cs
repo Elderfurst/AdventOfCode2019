bec0395c67d9c251e17aa2f7d51b3447c3d94e53
Nicholas Anderson <njcanderson@gmail.com>
2019-12-01 07:47:05 -0800

    Day 1 solutions part 1 and 2

--- AdventOfCode2019/Day1.cs
+++ AdventOfCode2019/Day1.cs
@@ -1,14 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.IO;
+using System.Linq;
+using System;
 
 namespace AdventOfCode2019
 {
     public class Day1
     {
+        private static readonly double[] _inputs = File.ReadAllText(@"Inputs/Day1.txt").Split('\n').Select(double.Parse).ToArray();
         public void Run()
         {
+            PartOne();
+            PartTwo();
+        }
+
+        private void PartOne()
+        {
+            var totalFuel = 0.0;
+
+            foreach (var input in _inputs)
+            {
+                totalFuel += Math.Floor(input / 3) - 2;
+            }
+
+            Console.WriteLine(totalFuel);
+        }
+
+        private void PartTwo()
+        {
+            var totalFuel = 0.0;
+
+            foreach (var input in _inputs)
+            {
+                totalFuel += CalculateFuel(input);
+            }
+
+            Console.WriteLine(totalFuel);
+        }
+
+        private double CalculateFuel(double input)
+        {
+            var fuel = Math.Floor(input / 3) - 2;
             
+            if (fuel <= 0)
+            {
+                return 0;
+            }
+
+            return fuel + CalculateFuel(fuel);
         }
     }
 }
