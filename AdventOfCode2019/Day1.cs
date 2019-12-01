151cf21cdbb1cc56ebc47b3785a198efe7c06804
Nicholas Anderson <njcanderson@gmail.com>
2019-12-01 07:28:33 -0800

    Scaffold base 2019 project

---
+++ AdventOfCode2019/Day1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode2019
+{
+    public class Day1
+    {
+        public void Run()
+        {
+            
+        }
+    }
+}
---
+++ AdventOfCode2019/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace AdventOfCode2019
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var run = new Day1();
+            run.Run();
+        }
+    }
+}
