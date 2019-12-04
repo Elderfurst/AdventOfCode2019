256c090f4c29f31bc300f5e6e1d2dabd2637e4d3
Nicholas Anderson <njcanderson@gmail.com>
2019-12-04 07:59:09 -0800

    Day 4 Part 1 Solution

---
+++ AdventOfCode2019/Day4.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+
+namespace AdventOfCode2019
+{
+    public class Day4
+    {
+        private readonly int _start = 245318;
+        private readonly int _end = 765747;
+        public void Run()
+        {
+            PartOne();
+            PartTwo();
+        }
+
+        private void PartOne()
+        {
+            var totalValidPasswords = 0;
+
+            for (var i = _start; i <= _end; i++)
+            {
+                if (ValidateNumber(i))
+                {
+                    totalValidPasswords++;
+                }
+            }
+
+            Console.WriteLine(totalValidPasswords);
+        }
+
+        private void PartTwo()
+        {
+
+        }
+
+        private bool ValidateNumber(int number)
+        {
+            var numberArray = number.ToString().Select(x => x.ToString()).ToArray();
+
+            var sequential = false;
+
+            for (var i = 1; i < numberArray.Length; i++)
+            {
+                var previousInt = int.Parse(numberArray[i - 1]);
+                var currentInt = int.Parse(numberArray[i]);
+
+                if (currentInt < previousInt)
+                {
+                    return false;
+                }
+
+                if (currentInt == previousInt)
+                {
+                    sequential = true;
+                }
+            }
+
+            return sequential;
+        }
+    }
+}
--- AdventOfCode2019/Program.cs
+++ AdventOfCode2019/Program.cs
@@ -4,7 +4,7 @@
     {
         public static void Main()
         {
-            var run = new Day3();
+            var run = new Day4();
             run.Run();
         }
     }
