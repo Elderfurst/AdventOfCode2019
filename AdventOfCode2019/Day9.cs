d0251d2c9d960b8efaba20de9aa52f0a501b5a2b
Nicholas Anderson <njcanderson@gmail.com>
2019-12-09 07:51:18 -0800

    Day 9 Start

---
+++ AdventOfCode2019/Day9.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2019
+{
+    class Day9
+    {
+        private static readonly int[] Inputs = File.ReadAllText(@"inputs/Day9.txt").Split(',').Select(int.Parse).ToArray();
+        public void Run()
+        {
+            PartOne();
+            PartTwo();
+        }
+
+        private void PartOne()
+        {
+
+        }
+
+        private void PartTwo()
+        {
+
+        }
+    }
+}
