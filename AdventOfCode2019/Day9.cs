e83badcd2f6d3ffddc967b8b170977bbc56a26bf
Nicholas Anderson <njcanderson@gmail.com>
2019-12-10 07:55:16 -0800

    Day 9 Part 1 first attempt

--- AdventOfCode2019/Day9.cs
+++ AdventOfCode2019/Day9.cs
@@ -7,7 +7,7 @@ namespace AdventOfCode2019
 {
     class Day9
     {
-        private static readonly int[] Inputs = File.ReadAllText(@"inputs/Day9.txt").Split(',').Select(int.Parse).ToArray();
+        private static readonly long[] Inputs = File.ReadAllText(@"inputs/Day9.txt").Split(',').Select(long.Parse).ToArray();
         public void Run()
         {
             PartOne();
@@ -16,7 +16,22 @@ namespace AdventOfCode2019
 
         private void PartOne()
         {
+            var computer = new IntCodeComputer(Inputs)
+            {
+                DiagnosticInputs = new List<long>
+                {
+                    1
+                }
+            };
 
+            var output = 0L;
+
+            while (output != -1)
+            {
+                output = computer.CalculateIntCode();
+
+                Console.WriteLine(output);
+            }
         }
 
         private void PartTwo()
